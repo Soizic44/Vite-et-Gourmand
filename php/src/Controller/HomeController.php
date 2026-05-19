42f3d50338593e1289e3541a99055e48abe42a4a
Soizic44 <fetiveau.soizic1@hotmail.fr>
2026-05-19 19:29:06 +0200

    création controller test "HomeController" avec reponse en fichier html.twig à transfomer en réponse json et "CommandeController"

--- php/src/Controller/HomeController.php
+++ php/src/Controller/HomeController.php
@@ -1,5 +1,5 @@
 <?php
-
+// src/Controller/HomeController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,9 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 require_once __DIR__ . '/../Database/connexion.php';
 
+#[Route('/commande', name: 'commande_')]
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(): Response
     {
         $pdo = getPDO();
@@ -25,4 +26,38 @@ class HomeController extends AbstractController
             'commandes' => $commandes
         ]);
     }
+
+    #[Route('/create', name: 'new', methods: ['POST'])]
+    public function new(): Response
+    {
+        // … Crée une nouvelle commande et la sauvegarde en base de données
+        return new Response('Nouvelle commande');
+    }
+
+    #[Route('/show/{id}', name: 'show', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        // ... Affiche les détails de la commande
+        return $this->json([
+            'message' => 'Commande trouvée',
+            'id' => $id
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit', methods: ['PUT'])]
+    public function edit(int $id): Response
+    {
+        // … Edite la commande et le sauvegarde en base de données
+        return new Response('Commande modifiée');
+    }
+
+    #[Route('/delete/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(int $id): Response
+    {
+        // ... Supprime la commande de la base de données
+        return new Response('Commande supprimée');
+    }
 }
+
+
+
